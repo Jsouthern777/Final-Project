c89260b86738a80aefe492f9a6c3ac79eca02269
Jackson <100294773+Jsouthern777@users.noreply.github.com>
2024-11-26 15:00:10 -0500

    Email validation ensuring GCC email addresses.

--- loginforms.py
+++ loginforms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import PasswordField, SubmitField, EmailField, SelectField, StringField
-from wtforms.validators import InputRequired, Email, EqualTo, Length, Optional
+from wtforms.validators import InputRequired, Email, EqualTo, Length, Optional, ValidationError
 from enum import Enum
 
 class Role(Enum):
@@ -8,11 +8,15 @@ class Role(Enum):
     Editor = 2
     Admin = 3
 
+#custom validator for gcc email addresses
+def gcc_email_validator(form, field):
+    if not field.data.endswith('@gcc.edu'):
+        raise ValidationError("Email must be a Grove City College email address")
 # define our own FlaskForm subclass for our form
 class RegisterForm(FlaskForm):
     firstName = StringField("First Name :", validators=[InputRequired()])
     lastName = StringField("Last Name :", validators=[InputRequired()])
-    email = EmailField("Email: ", validators=[InputRequired(), Email()])
+    email = EmailField("Email: ", validators=[InputRequired(), Email(), gcc_email_validator])
     password = PasswordField("Password: ", 
         validators=[InputRequired(), Length(min=8, max=256)])
     confirm_password = PasswordField("Confirm Password: ", 
@@ -28,3 +32,5 @@ class LoginForm(FlaskForm):
         validators=[InputRequired(), Length(min=8, max=256)])
     verification_password = PasswordField("Verification Password:")
     submit = SubmitField("Login")
+
+
